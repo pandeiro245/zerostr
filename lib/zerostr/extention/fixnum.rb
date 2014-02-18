1f64a24a5f1e564e69dfdca0f64cd3f1fc4a6766
Hironobu Nishikokura <nishiko@mindia.jp>
2014-02-18 23:42:20 +0900

    user a operator of %

--- lib/zerostr/extention/fixnum.rb
+++ lib/zerostr/extention/fixnum.rb
@@ -1,5 +1,5 @@
 class Fixnum
   def to_zerostr(limit=2)
-    self.to_s.to_zerostr(limit)
+    "%0#{limit}.0f" % self
   end
 end
--- lib/zerostr/extention/string.rb
+++ lib/zerostr/extention/string.rb
@@ -1,7 +1,5 @@
 class String
   def to_zerostr (limit=2)
-    num = limit - self.length
-    zeros = num > 0 ? "0" * num : ''
-    zeros + self
+    self.to_i.to_zerostr
   end
 end
